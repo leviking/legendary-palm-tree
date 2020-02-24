409b6a45d187432826979f2519395431fa11a3b9
[Eliot Sanford] <eliot587@gmail.com>
2020-02-24 17:00:27 -0600

    assignment: combine work into single delete and update sql

--- sanford/delete.sql
+++ sanford/delete.sql
@@ -3,4 +3,7 @@
 
 USE ncaa_fb;
 DELETE FROM player WHERE id=3;
+DROP TABLE player;
+DROP DATABASE ncaa_fb;
+SELECT * from player;
 SELECT * FROM player;
--- sanford/update.sql
+++ sanford/update.sql
@@ -21,6 +21,10 @@ INSERT into player (
 ALTER TABLE player
 ADD nickname varchar(20);
 
+UPDATE player 
+SET nickname = 'Dak'
+WHERE id = 2;
 
+SELECT nickname FROM player WHERE id = 2;
 
 SELECT * FROM player;
