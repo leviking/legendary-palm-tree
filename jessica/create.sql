b9fdadb3a3ab797f782403f1c56dfe2bdf785798
Jessica Nelson <jnelson151988@gmail.com>
2020-02-24 18:53:33 -0600

    added jessica's files

---
+++ jessica/create.sql
@@ -0,0 +1,9 @@
+create table schools(
+    id int auto_increment primary key,
+    schoolname varchar(30),
+    city varchar(30),
+    state varchar(20),
+    discipline varchar(20),
+    students mediumint
+);
+
