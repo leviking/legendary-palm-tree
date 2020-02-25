8430b4a033f92397569afb8cdb490c45fe3019c6
levi <levi@mscode.dev>
2020-02-24 18:26:39 -0600

    refactor: split file into separate files
    This standardizes our files to follow the convention
    of putting the appropriate functions for create,
    read, update, and delete in files of the same name.

--- levi/create.sql
+++ levi/create.sql
@@ -4,8 +4,8 @@ drop database attendance;
 --CRUD for the database
 
 --Create
-create database attendance; 
-use attendance;
+create database attendance;
+use attendance; 
 
 create table students(
     id int not null auto_increment,
@@ -31,32 +31,6 @@ create table attendance_logs(
     foreign key(student_id) references students(id)
 );
 
--- READ
-
-show tables;
-desc students;
-desc attendance_logs;
-
---update
-alter table students
-add `last_name` varchar(24) after `name`;
-
-alter table students
-change name first_name varchar(24);
-
-alter table attendance_logs
-add lat float after attendance_date;
-
-alter table attendance_logs
-add lng float after lat;
-
-desc attendance_logs;
-
---delete
--- drop table attendance_logs;
--- drop table students;
--- show tables;
-
 --Table level create
 insert into students(
     first_name,
@@ -69,18 +43,3 @@ insert into students(
     now(),
     'jb@mscode.dev'
 );
-
---Read
-select * from students;
-
---Update
-update students
-set deleted_at=now()
-where id=1;
-
---Delete
-delete from students
-where id=1;
-
---read again just to be sure
-select * from students;
---
+++ levi/delete.sql
@@ -0,0 +1,9 @@
+--delete
+delete from students
+where id=1;
+
+drop table attendance_logs;
+
+drop table students;
+
+show tables;
