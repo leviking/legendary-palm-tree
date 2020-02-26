2d5f7419899bbab64b4737ede93b75ddb7e9626f
FearBlackScarab <fearblackscarab@gmail.com>
2020-02-25 20:00:35 -0600

    updated meme to have auto_increment id, and defined salt

--- jordan/create.sql
+++ jordan/create.sql
@@ -1,11 +1,14 @@
+-- create database
 create database lol;
-
+-- enter database and create tables
 use lol;
-create table meme (name varchar(20), creator varchar(20));
-create table gif (name varchar(20), creator varchar(20));
-create table hashtag (name varchar(26));
-
-insert into meme(name, creator) values ('minion', 'bob');
-insert into gif(name, creator) values ('minion', 'bob');
-insert into hashtag(name) values ('bob');
-insert into hashtag(name) values ('minion');
+-- meme table
+create table meme (meme_name varchar(20) primary key, meme_creator varchar(20));
+-- hashtag table
+create table hashtag (hashtag_name varchar(20));
+-- working on setting hashtag as a foreign key from meme_name
+-- insert values into meme
+insert into meme(meme_name, meme_creator) values ('minion', 'bob');
+insert into meme(meme_name, meme_creator) values ('tough', 'shane');
+insert into meme(meme_name, meme_creator) values ('weak', 'clam');
+insert into meme(meme_name, meme_creator) values ('class', 'dan');
--- jordan/update.sql
+++ jordan/update.sql
@@ -1,1 +1,4 @@
-alter table meme add salt varchar(20)
+-- added salt to meme table
+alter table meme add salt varchar(20);
+-- defining the salt
+update meme set salt ='TarTarHar'
